670c2447625756b4fc29d3e3699735dcae1032b0
smgd <soapmangoesdown@gmail.com>
2018-11-24 18:36:53 +0300

    added some helping functions

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import os
 app = Flask(__name__)
 UPLOAD_DIR = '/home/soapman/Documents/code/py/fileapi/storage'
 app.config['DIR'] = UPLOAD_DIR
-file_base = []
 
 def get_hash_md5(file):
     m = hashlib.md5()
@@ -17,9 +16,21 @@ def get_hash_md5(file):
     file.seek(0)
     return m.hexdigest()
 
-def save_file(file):
+def make_path(file):
     os.mkdir(app.config['DIR'] + '/' + file[:2])
     return os.path.join(app.config['DIR'], file[:2], file)
+
+def existance(file_hash):
+    path = os.path.join(app.config['DIR'], file_hash[:2], file_hash)
+    return os.path.exists(path), path
+
+def show_files():
+    for root, subdirs, files in os.walk(app.config['DIR']):
+        if files != []:
+            for file in files:
+                yield file
+
+file_base = [file for file in show_files()]
 
 @app.route('/')
 def home():
@@ -31,19 +42,17 @@ def files():
         if not request.args:
             return jsonify(file_base)
         elif 'hash' in request.args:
-            path = os.path.join(app.config['DIR'], request.args['hash'][:2], request.args['hash'])
-            if os.path.exists(path):
-            # print(path)
+            file_status, path = existance(request.args['hash'])
+            if file_status:
                 return send_file(path, as_attachment=True)
-            return jsonify({'you_better': 'than_most', 'you_can': 'download_file', 'hash': request.args['hash']})
+            else:
+                return jsonify({'error': 'file_not_found'})
     
     if request.method == 'POST':
-        
         f = request.files['file']
-        print(f.read(2))
         hash_filename = get_hash_md5(f)
         if hash_filename not in file_base:
-            path = save_file(hash_filename)
+            path = make_path(hash_filename)
             f.save(path)
             file_base.append(hash_filename)
             return jsonify({'you_have_uploaded': 'file', 'hash': hash_filename})
@@ -53,11 +62,14 @@ def files():
     if request.method == 'DELETE':
         file = request.args['hash']
         os.remove(os.path.join(app.config['DIR'], file[:2], file))
+        file_base.remove(file)
         try:
             os.rmdir(os.path.join(app.config['DIR'], file[:2]))
         except:
             pass
         return jsonify({'you_have': 'deleted_file'})
+
+
 
 if __name__ == '__main__':
     app.run()
